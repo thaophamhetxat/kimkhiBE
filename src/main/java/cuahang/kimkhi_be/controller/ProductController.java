432b475f46862fc13912c298f76ab7625047747f
thethao <66192167+thaophamhetxat@users.noreply.github.com>
2021-12-29 23:23:05 +0700

    swagger + logger

--- src/main/java/cuahang/kimkhi_be/controller/ProductController.java
+++ src/main/java/cuahang/kimkhi_be/controller/ProductController.java
@@ -4,7 +4,11 @@ import cuahang.kimkhi_be.dto.response.ResponseMessage;
 import cuahang.kimkhi_be.model.Cart;
 import cuahang.kimkhi_be.model.Product;
 import cuahang.kimkhi_be.service.impl.IProductService;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -12,15 +16,24 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
+@Aspect
 @RestController
 @RequestMapping("/kimkhi/sanphams/")
 @CrossOrigin("*")
 public class ProductController {
+    private Logger logger = LoggerFactory.getLogger(ProductController.class);
+
+    @Value("${uploadPart}")
+    String uploadPart;
 
     @Autowired
     private IProductService iProductService;
@@ -28,10 +41,13 @@ public class ProductController {
     //Xắp xếp theo tên,phân trang
     @GetMapping("/page")
     public ResponseEntity<?> pageProduct(@PageableDefault(sort = "nameProduct", direction = Sort.Direction.ASC) Pageable pageable) {
+        logger.info("start check list:");
         Page<Product> productPage = iProductService.findAll(pageable);
         if (productPage.isEmpty()) {
+            logger.info("list null");
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         }
+        logger.info("show list:" + productPage);
         return new ResponseEntity<>(productPage, HttpStatus.OK);
     }
 
@@ -45,8 +61,9 @@ public class ProductController {
     }
 
     @PostMapping
-    public ResponseEntity<?> add(@RequestBody Product product) {
+    public ResponseEntity<?> add(@RequestBody Product product) throws IOException {
         //xét trừng lặp => return về message
+        logger.info("start:");
         if (iProductService.existsByNameProduct(product.getNameProduct())) {
             return new ResponseEntity<>(new ResponseMessage("no_name_product"), HttpStatus.OK);
         }
@@ -54,7 +71,12 @@ public class ProductController {
         if (product.getImage() == null) {
             return new ResponseEntity<>(new ResponseMessage("no_image"), HttpStatus.OK);
         }
+//        String name = image.getOriginalFilename();
+//        logger.info("check"+name);
+//        FileCopyUtils.copy(image.getBytes(), new File(uploadPart + name));
+
         iProductService.addProduct(product);
+
         return new ResponseEntity<>(new ResponseMessage("add_success"), HttpStatus.OK);
     }
 
